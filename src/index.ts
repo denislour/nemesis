2861b60432f20ce856f383f33fd046a86fb86d88
Lektor <denislour@gmail.com>
2022-09-27 23:34:53 +0700

    Integrate Koa

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("Hello World");
+import Koa, { Context, Next } from "koa";
+import Router from "koa-router";
+import morgan from "koa-morgan";
+
+const app = new Koa();
+const router = new Router();
+app.use(morgan("dev")).use(router.routes()).use(router.allowedMethods());
+
+router.get("/", (ctx: Context, next: Next) => {
+  ctx.body = "Hello World";
+});
+
+const port = Number(process.env.PORT) ?? 8000;
+app.listen(port, "0.0.0.0", () => {
+  console.log(`Server started at http://localhost:${port}`);
+});
